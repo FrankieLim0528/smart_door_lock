ef2d7b4376deeda4e85f0bf077a6b89c317a2407
richiephang <richiphang01@gmail.com>
2023-05-29 14:16:22 +0800

    speech recognition and input passphrase module

--- scripts/speech_recognition_node.py
+++ scripts/speech_recognition_node.py
@@ -3,7 +3,7 @@
 import rospy
 from std_msgs.msg import String
 import speech_recognition as sr
-
+import time
 # recognize speech in audio file
 # audio_file = "path/to/audio/file.wav"
 # audio = sr.AudioFile(audio_file)
@@ -14,7 +14,7 @@ import speech_recognition as sr
 def googlesr():
     rospy.init_node('googlesr', anonymous=True)
     pub = rospy.Publisher('result', String, queue_size=10)
-
+    count = 3
     while not rospy.is_shutdown():
         # obtain audio from the microphone
         r = sr.Recognizer()
@@ -23,16 +23,18 @@ def googlesr():
         # get available microphones
         # print(sr.Microphone.list_microphone_names())
         # mic = sr.Microphone(device_index=3)
-
+        file_path = r"/home/mustar/catkin_ws/src/smart_door_lock/passphrase.txt"
+        with open(file_path, "r") as f:
+            passphrase = f.readline()
         with sr.Microphone() as source:
             print(">>> Say something!")
-            r.adjust_for_ambient_noise(source, duration=5)
-            audio = r.listen(source, phrase_time_limit=3)
+            # r.adjust_for_ambient_noise(source, duration=5)
+            audio = r.listen(source)
             #audio = r.record(source, duration=5)
         
         # write audio to a WAV file
-        with open("microphone-results.wav", "wb") as f:
-            f.write(audio.get_wav_data())
+        # with open("microphone-results.wav", "wb") as f:
+        #     f.write(audio.get_wav_data())
         
         # audio_file = sr.AudioFile("microphone-results.wav")
         # with audio_file as source:
@@ -40,16 +42,28 @@ def googlesr():
         #     r.recognize_google(audio_data=audio_file)
 
         # recognize speech
+        result = ""
         try:
-            # result = r.recognize_google(audio) # online using Google Web Speech API
-            result = r.recognize_sphinx(audio) # offline using CMUSPhinx
+            result = r.recognize_google(audio) # online using Google Web Speech API
             print("SR result: " + result)
+            if result == passphrase:
+            # result = r.recognize_sphinx(audio) # offline using CMUSPhinx
+                # TODO speech generation
+                print("Door unlocked!")
+                break
+            else:
+                # TODO speech generation
+                print("Wrong passphrase, please try again")
+                count -= 1
+            if count == 0:
+                # TODO speech generation
+                print("You have exceeded the maximum number of tries, please try again 10 minutes later.")
+                time.sleep(10)
+            pub.publish(result)
         except sr.UnknownValueError:
             print("SR could not understand audio")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
-        
-        pub.publish(result)
 
 if __name__ == '__main__':
     try:
